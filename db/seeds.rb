e701a30052ce8e3eb48b66fa59fcb261216bacab
Saile Marinho saile-ribeiro@hotmail.com <saile-ribeiro@hotmail.com>
2019-05-22 16:57:27 -0300

    Add restaurant and review controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '876-876-983',
+    category: 'italian'
+
+  },
+  {
+    name: 'Pizza East',
+    address: 'brazil E1 6PQ',
+    phone_number: '876-876-983',
+    category: 'italian'
+  },
+  {
+    name: 'Jesus pizza',
+    address: '7 Boundary St, France 7JE',
+    phone_number: '666-856-983',
+    category: 'french'
+
+  },
+  {
+    name: 'Soho',
+    address: 'utrecht High St, London E1 6PQ',
+    phone_number: '975-876-983',
+    category: 'japanese'
+  },
+  {
+    name: 'cao veio',
+    address: 'sp St, London E2 7JE',
+    phone_number: '876-729-983',
+    category: 'belgian'
+
+  },
+  {
+    name: 'ki mukeca',
+    address: 'salvador 6PQ',
+    phone_number: '666-666-666',
+    category: 'chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
